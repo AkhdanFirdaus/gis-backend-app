2e788b9336a43692a6eb316a2672b99acadd87af
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2022-11-01 20:36:02 +0700

    Init project, dependencies, and setup environment

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config({path: './.env'})
+
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const path = require('path')
+
+const app = express()
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'assets', 'uploads')))
+
+app.listen(8888, () => {
+  console.log('App Listening on port 8888')
+})
+
