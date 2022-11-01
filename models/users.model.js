cc75285bc8eb84b6bc04d21d3601a193fff589de
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2022-11-01 20:55:12 +0700

    [ADD] user model

---
+++ models/users.model.js
@@ -0,0 +1,44 @@
+const db = require('../helpers/db')
+
+const table = 'users'
+
+exports.insertUser = (data)  => {
+  const sql = `INSERT INTO "${table}" ("email", "password") VALUES ($1, $2) RETURNING *`
+  const params = [data.email, data.password]
+  return db.query(sql, params)
+}
+
+exports.selectAllUser = (data) => {
+  const sql = `SELECT * FROM "${table}" WHERE "${data.searchBy}" LIKE '%${data.search}%' ORDER BY "${data.sortBy}" ${data.reverse ? 'DESC' : 'ASC'} LIMIT $1 OFFSET $2`
+  const params = [data.limit, data.offset]
+  return db.query(sql, params)
+}
+
+exports.selectAll = (data) => {
+  const sql = `SELECT * FROM "${table}" WHERE "${data.searchBy}" LIKE '${data.search}'`
+  return db.query(sql)
+}
+
+exports.selectUserById = (id) => {
+  const sql = `SELECT * FROM "${table}" WHERE id = $1`
+  const params = [id]
+  return db.query(sql, params)
+}
+
+exports.selectUserByEmail = (email) => {
+  const sql = `SELECT * FROM "${table}" WHERE email = $1`
+  const params = [email]
+  return db.query(sql, params)
+}
+
+exports.updateUserById = (id, data) => {
+  const sql = `UPDATE "${table}" SET email=$2, password=$3 WHERE id = $1 RETURNING *`
+  const params = [id, data.email, data.password]
+  return db.query(sql, params)
+}
+
+exports.deleteUserById = (id) => {
+  const sql = `DELETE FROM "${table}" WHERE id = $1`
+  const params = [id]
+  return db.query(sql, params)
+}
